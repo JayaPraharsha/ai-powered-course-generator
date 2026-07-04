49c3d77d084b1a7b8f484765f1c65c6427698f5a
unknown <jayapraharsha@gmail.com>
2026-07-04 16:05:23 +0530

    Redesign nav shell: auth-aware sidebar/topbar, mobile drawer

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,7 +4,7 @@ import Topbar from "./Topbar"
 
 function Layout() {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex min-h-screen bg-slate-50">
       <Sidebar />
       <div className="flex min-w-0 flex-1 flex-col">
         <Topbar />
